bc9c5e1a0b5d6b9ea8844c00d838ba3eb68164cd
Anton Kalenskyi <98702650+AntonKalenskiy@users.noreply.github.com>
2023-08-25 17:50:38 +0300

    Added updating of the books to controller and service layers. Added field isDeleted to the Model, safe delete to the proj.

--- src/main/java/com/springframework/boot/onlinebookstore/controller/BookController.java
+++ src/main/java/com/springframework/boot/onlinebookstore/controller/BookController.java
@@ -5,11 +5,15 @@ import com.springframework.boot.onlinebookstore.dto.CreateBookRequestDto;
 import com.springframework.boot.onlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,5 +35,17 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBookById(@PathVariable Long id,
+                                  @RequestBody CreateBookRequestDto bookRequestDto) {
+        return bookService.updateById(id, bookRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
--- src/main/java/com/springframework/boot/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/springframework/boot/onlinebookstore/service/impl/BookServiceImpl.java
@@ -8,6 +8,7 @@ import com.springframework.boot.onlinebookstore.model.Book;
 import com.springframework.boot.onlinebookstore.repository.BookRepository;
 import com.springframework.boot.onlinebookstore.service.BookService;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -33,7 +34,30 @@ public class BookServiceImpl implements BookService {
     @Override
     public BookDto findById(Long id) {
         return bookMapper.toDto(bookRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("Can`t find book by id: " + id))
+                () -> new EntityNotFoundException("Book with id: " + id + " not found"))
         );
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public BookDto updateById(Long id, CreateBookRequestDto bookRequestDto) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book existingBook = optionalBook.get();
+            existingBook.setTitle(bookRequestDto.getTitle());
+            existingBook.setAuthor(bookRequestDto.getAuthor());
+            existingBook.setIsbn(bookRequestDto.getIsbn());
+            existingBook.setPrice(bookRequestDto.getPrice());
+            existingBook.setDescription(bookRequestDto.getDescription());
+            existingBook.setCoverImage(bookRequestDto.getCoverImage());
+            Book savedBookInDB = bookRepository.save(existingBook);
+            return bookMapper.toDto(savedBookInDB);
+        } else {
+            throw new EntityNotFoundException("Book with id: " + id + " not found");
+        }
     }
 }
