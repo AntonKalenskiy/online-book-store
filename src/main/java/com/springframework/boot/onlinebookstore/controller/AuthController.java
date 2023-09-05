c23204d384c306b35aa6f1b6f551206e8abb8b29
Anton Kalenskyi <98702650+AntonKalenskiy@users.noreply.github.com>
2023-09-05 16:54:00 +0300

    Fixed checkstyle mistakes

--- src/main/java/com/springframework/boot/onlinebookstore/controller/AuthController.java
+++ src/main/java/com/springframework/boot/onlinebookstore/controller/AuthController.java
@@ -9,7 +9,6 @@ import com.springframework.boot.onlinebookstore.security.AuthenticationService;
 import com.springframework.boot.onlinebookstore.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
