e4287005b0d57bc586eb855c0cae07344cc6b45b
Anton Kalenskyi <98702650+AntonKalenskiy@users.noreply.github.com>
2023-09-13 16:25:00 +0300

    Added Order and OrderItem

---
+++ src/main/java/com/springframework/boot/onlinebookstore/model/Order.java
@@ -0,0 +1,51 @@
+package com.springframework.boot.onlinebookstore.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "orders")
+@SQLDelete(sql = "UPDATE orders SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @Column(nullable = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true)
+    private Status status;
+    @Column(nullable = false)
+    private BigDecimal total;
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+    @Column(nullable = false)
+    private String shippingAddress;
+    @OneToMany
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JoinTable(name = "orders_order_items",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "order_item_id"))
+    private Set<OrderItem> orderItems;
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+
+    public enum Status{
+        DELIVERED, COMPLETED, PENDING
+    }
+
+}
---
+++ src/main/java/com/springframework/boot/onlinebookstore/model/OrderItem.java
@@ -0,0 +1,7 @@
+package com.springframework.boot.onlinebookstore.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class OrderItem {
+}
